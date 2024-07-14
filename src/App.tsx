bb10fa49c5de7f0c2f8a3a80e4083dbbf80cad86
Anant-Kovil <75954126+Anant-Kovil@users.noreply.github.com>
2024-07-14 19:06:22 -0400

    Added config for SPA

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { HashRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { Main } from "./pages/main";
 import { Navbar } from './components/navbar/navbar';
 import { Projects } from './pages/projects';
