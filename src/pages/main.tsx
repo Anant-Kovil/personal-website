478899eace25d301b75542008d8b737e6db37ca1
Anant-Kovil <75954126+Anant-Kovil@users.noreply.github.com>
2024-11-12 13:52:03 -0800

    Updater Website (removed email + added sunny)

--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -5,9 +5,9 @@ export const Main = () => {
   return (
     <Container>
       <div>
-        <Typography>👋 Hey, hope your day is going well</Typography>
-        <Typography mt={2}>I'm a Engineer + Designer, exploring the future of human augmentation through wearable devices. I'm also a 4th year student studying CS + Business.</Typography>
-        <Typography mt={2}>Current areas of interest:</Typography>
+        <Typography>👋 Hey, hope your day is going well. I'm a Engineer + Designer, exploring the future of human augmentation through devices. I'm also a 4th year student studying CS + Business.</Typography>
+        <Typography mt={3}>Building Sunny, an Intelligent Companion designed for elders. Visit <a href="https://sunnycares.org">sunnycares.org</a> to learn more</Typography>
+        <Typography mt={3}>Current areas of interest:</Typography>
         <Typography sx={{fontWeight:'300'}}>• Neurotech</Typography>
         <Typography sx={{fontWeight:'300'}}>• Forms of Communication</Typography>
         <Typography sx={{fontWeight:'300'}}>• Biosensors</Typography>
@@ -17,7 +17,6 @@ export const Main = () => {
         <Typography sx={{fontWeight:'300'}}>• Design/Art/anything creative</Typography>
         <Typography sx={{fontWeight:'300'}}>• Breathwork (hmu for recs)</Typography>
         <Typography sx={{fontWeight:'500'}} mt={2}>Best ways to reach me</Typography>
-        <Typography sx={{fontWeight:'300'}}>anant.k1226 (at) gmail (dot) com</Typography>
         <Typography sx={{ml:3, mt:1, mb:1.5, fontWeight:'300'}}>or</Typography>
         <Chip sx={{fontWeight:'300'}}label="Twitter" color="primary"variant="filled" onClick={()=>{window.open('https://x.com/Anant_world')}} />
       </div>
